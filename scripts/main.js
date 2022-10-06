848af5ea2ca2d5147173a22aa6a7ac440b9be4f4
jtslemon <100393946+jtslemon@users.noreply.github.com>
2022-10-06 16:48:23 +0900

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -32,10 +32,32 @@ const promotionEl = document.querySelector('section.promotion');
 const promotionToggleBtn = document.querySelector('.toggle-promotion');
 // 토글 버튼을 클릭했을 때
 promotionToggleBtn.addEventListener('click', function () {
-    if(promotionEl.classList.contains('hide')){
+    if(promotionEl.classList.contains('hide')){ // 만약 promotion클래스에 hide서식이 있다면
         promotionEl.classList.remove('hide');
     }
     else{
         promotionEl.classList.add('hide');
     }
-})
+});
+
+gsap.to('.m_img1', 1.5, {
+    delay: .7,
+    y: 15,
+    repeat: -1,
+    yoyo: true,
+    ease: Power1.easeInOut
+});
+gsap.to('.m_img2', 2, {
+    delay: .3,
+    y: -15,
+    repeat: -1,
+    yoyo: true,
+    ease: Power1.easeInOut
+});
+gsap.to('.m_img3', 2.5, {
+    delay: .1,
+    y: 15,
+    repeat: -1,
+    yoyo: true,
+    ease: Power1.easeInOut
+});
